2cd9e1758504c49af6abece3cd261f334d76d4c0
stephaneSBO <stephane.boulent@gmail.com>
2017-12-22 10:16:06 +0100

    maj

--- convert.sh
+++ convert.sh
@@ -18,7 +18,7 @@ do
   `pandoc -f docbook $f -o $filename.md`
 
 # Suppression des fichiers xml  
-rm $f
+#rm $f
 done
 
 # copie des images
@@ -27,3 +27,19 @@ cp -p ../doc/images/* images/
 
 #Optimation fichier md (image path)
 sed -i 's/..\/images/images/g' *.md
+
+#variable icon name
+#cat ../plugin_info/info.json | grep '"id" :' |awk '{ print $3}' | sed s/\"//g | sed s/,//g | sed 's/$/_icon.png/' | sed 's/^/nameLogo: /' >> _config.yml
+end=_icon.jpg
+#echo $end
+idname=$(cat plugin_info/info.json | grep '"id" :'|awk '{ print $3}' | sed s/\"//g | sed s/,//g | sed 's/^/nameLogo: /' | dos2unix)$end
+#echo $idname
+sed -i '/nameLogo/c\'"$idname"'' docs/_config.yml
+
+# correction des %22 interprété par github page
+sed -i 's/%22/"/g' *.md
+
+# add version
+sed -i '/version:/d' _config.yml
+date +"%Y%m%d%H%M" | sed 's/^/version: /' >> _config.yml
+
